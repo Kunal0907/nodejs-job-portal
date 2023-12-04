e6a1a0a1550c141a0f62ff08c0597c4942827a60
Kunal Gosavi <110988258+Kunal0907@users.noreply.github.com>
2023-12-04 17:01:08 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Connected to Mongodb database ${mongoose.connection.host}`);
+    console.log(`Connected to Mongodb Database ${mongoose.connection.host}`);
   } catch (error) {
     console.log(`MongoDB Error ${error}`);
   }
